ac906efc90ee20418e55a94690cd3763515b796f
Dammmmilare <josh.oyed@gmail.com>
2025-03-17 04:26:11 -0600

    Initial push

--- src/goph420-lab03/Root-Finding-function.py
+++ src/goph420-lab03/Root-Finding-function.py
@@ -28,8 +28,26 @@ def root_newton_raphson(x0, f, dfdx, tol=1e-6, max_iter=100):
     max_iter : The fifth return value (type int) should be the maximum number of iterations.
 
     """
+    errors = []
+    x = x0 # Initial guess
+    for i in range(max_iter):
+        fx = f(x)
+        dfx = dfdx(x)
 
+        if abs(dfx) < 1e-12:
+            raise ValueError("Derivative is close to zero")
+        break
 
+    x_new = x - fx / dfx
+    error = abs((x_new - x) / x_new) if x_new ! = 0 else 0
+    errors.append(error)
+
+    if error < tol:
+        return x_new, i + 1, np.array(errors)
+    
+    x = x_new
+    return x, f(x), np.array(errors)
+    
 
 def root_secant_modified(x0, dx, f, tol=1e-6, max_iter=100):
     """
---
+++ tests/Asymptote_function.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Computing the function using the graphichal method to visualize our data points to clearly find the root of the function
+"""Maximum value equation: S_max = np.sqrt(H ** 2 * (B1 ** -2 - B2 ** -2)) and  Step size for constant F and multiple k values: S = (0.25 * f) * (2k + 1)"""
+    
+def Fz(Z, f):
+   # density (kg/m^3)
+   d1 = 1800
+   d2 = 2500
+
+   # velocity (m/s)
+   B1 = 1900
+   B2 = 3200
+
+   # Area thickness (m)
+   H = 4000
+   return np.tan(2 * np.pi() * f * Z) - (d2 / d1) * np.sqrt((H**2) * (B1 **2 - B2 **2) -Z **2) / Z
+
+def dFz(Z, f):
+   # density (kg/m^3)
+   d1 = 1800
+   d2 = 2500
+
+   # velocity (m/s)
+   B1 = 1900
+   B2 = 3200
+
+   # Area thickness (m)
+   H = 4000
+   return - p2/p1 * np.sqrt(H**2 * ( B1 ** -1 - B2)-Z**2) / Z**2 - p2/1 * 1/Z * (H**2 * B1 ** -2 - B2 ** -2 - Z**2)**0.5 - 2 * np.pi * f * 1/np.cos(2 * np.pi * f * Z)**2
+
+# Finding the asymptote of the function
+def asymptote_finder():
+    # Densities in kg/m^3
+    p1 = 1800
+    p2 = 2500
+
+    # Velocity (m/s)
+    B1 = 1900
+    B2 = 3200
+
+    # Area Thickness in (m)
+    H = 4000
+
+    # Computing the value of S_max
+    S_max = np.sqrt(H ** 2 * (B1 ** -2 - B2 ** -2))
+
+    # Initialize variables
+    S_list = []
+    k = 0  # Start from 0
+    f = 1
+    step = 1  # Define a small step size to increment k
+    print(S_max)
+
+    while k <= S_max:
+        S = (0.25 * 1/f) * (2 * k + 1)
+        if S > S_max:
+            break
+        else:
+            S_list.append(S)
+        k += step  # Increment k
+
+    return S_list  # Return the computed values
+
+# Call the function
+result = asymptote_finder()
+
+print(result) 
