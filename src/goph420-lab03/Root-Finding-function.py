83c301aee567b54b23e6c44d60ade958c69ae5d5
Dammmmilare <josh.oyed@gmail.com>
2025-03-17 04:28:04 -0600

    RFf done

--- src/goph420-lab03/Root-Finding-function.py
+++ src/goph420-lab03/Root-Finding-function.py
@@ -39,7 +39,7 @@ def root_newton_raphson(x0, f, dfdx, tol=1e-6, max_iter=100):
         break
 
     x_new = x - fx / dfx
-    error = abs((x_new - x) / x_new) if x_new ! = 0 else 0
+    error = abs((x_new - x) / x_new) if x_new != 0 else 0
     errors.append(error)
 
     if error < tol:
@@ -72,3 +72,23 @@ def root_secant_modified(x0, dx, f, tol=1e-6, max_iter=100):
     max_iter : The fifth return value (type int) should be the maximum number of iterations.
 
     """
+    errors = []
+    x = x0
+    for i in range(max_iter):
+        fx = f(x)
+        dfx = (f(x + dx) - fx) / dx 
+
+        if abs(dfx) < 1e-12:
+            raise ValueError("Derivative is close to zero")
+            
+
+        x_new = x - fx / dfx
+        error = abs((x_new - x) / x_new) if x_new != 0 else 0
+        errors.append(error)
+
+        if error < tol:
+            return x_new, i + 1, np.array(errors)
+
+        x = x_new
+        
+    return x, f(x), np.array(errors)
